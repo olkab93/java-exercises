6dddf00fa70b35ab9d3807eafbefac8b5028209c
Aleksandra Bochenska <aleksandra.bochenska@comarch.com>
2018-03-15 09:26:49 +0100

    ArrayBalance modified

--- src/main/java/pl/edu/agh/java/exercises/arrays/ArrayBalance.java
+++ src/main/java/pl/edu/agh/java/exercises/arrays/ArrayBalance.java
@@ -1,5 +1,8 @@
 package pl.edu.agh.java.exercises.arrays;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 /**
  * Given an array, return true if there is a place to split the array so that
  * the sum of the numbers on one side is equal to the sum of the numbers on the
@@ -12,20 +15,37 @@ package pl.edu.agh.java.exercises.arrays;
  */
 public class ArrayBalance {
     public boolean canBalance(int[] array) {
-        int sumLeft = 0;
-        int sumRight = 0;
-        for (int i = 0; i < array.length; i++) {
-            sumLeft += array[i];
+
+        int length = array.length;
+        boolean result = false;
+
+        if (length == 0 || length == 1) {
+            return true;
         }
 
-        for (int j = array.length - 1; j >= 0; j--) {
-            sumLeft -= array[j];
-            sumRight += array[j];
-            if (sumLeft == sumRight) {
-                return true;
+        for (int k = 0; k <= 100000; k++) {
+            Collections.shuffle(Arrays.asList(array));
+            int sumLeft = 0;
+            int sumRight = 0;
+            for (int i = 0; i < length; i++) {
+                sumLeft += array[i];
             }
+
+            for (int j = length - 1; j >= 0; j--) {
+                sumLeft -= array[j];
+                sumRight += array[j];
+                if (length % 2 != 0) {
+                    sumRight -= array[(length / 2) + 1];
+                }
+            }
+
+            if (sumLeft == sumRight) {
+                result = true;
+            } else {
+                result = false;
+            }
+            //throw new UnsupportedOperationException();
         }
-        return false;
-        //throw new UnsupportedOperationException();
+        return result;
     }
 }
